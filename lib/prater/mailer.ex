5b15e49fdfb990ca3967e3dd8d6e3cc76704c9ae
Noopur Sharma <noopurs0210@gmail.com>
2022-07-29 04:44:40 +0530

    Made the base project

---
+++ lib/prater/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Prater.Mailer do
+  use Swoosh.Mailer, otp_app: :prater
+end
---
+++ lib/prater_web/controllers/room_controller.ex
@@ -0,0 +1,8 @@
+defmodule PraterWeb.RoomController do
+  use PraterWeb, :controller
+
+  def index(conn, _params) do
+    rooms = Prater.Conversation.list_rooms()
+    render(conn, "index.html", rooms: rooms)
+  end
+end
